9a7aa4974a1d3194b581a8a7790f34a4667d92eb
jmg061 <jmg061@inlumine.ual.es>
2024-04-04 21:02:52 +0200

    Tarjeta

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { CardComponent } from './componentes/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent],
+  imports: [RouterOutlet, CabeceraComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ FRONTEND/src/app/componentes/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CardModule, ButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+}
