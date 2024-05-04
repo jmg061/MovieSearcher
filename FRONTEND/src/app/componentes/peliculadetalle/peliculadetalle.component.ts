56dd1c2eca53b4e0f377580b3a406aca73f6486f
jmg061 <jmg061@inlumine.ual.es>
2024-05-04 12:37:50 +0200

    a

--- FRONTEND/src/app/componentes/peliculadetalle/peliculadetalle.component.ts
+++ FRONTEND/src/app/componentes/peliculadetalle/peliculadetalle.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { pelicula } from '../../templates/pelicula';
 
 @Component({
   selector: 'app-peliculadetalle',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './peliculadetalle.component.scss'
 })
 export class PeliculadetalleComponent {
+
+  peli:pelicula = {
+    id: 1,
+    titulo: "patata",
+    imagen: "https://image.tmdb.org/t/p/w500/ivhOeG5S2CzKjcKhureKAtfonHg.jpg",
+    descripcion: "Ah",
+    fecha: "ayer mismo",
+    puntuacion: 10,
+    originaltitle: "Ah pero en ingles",
+  }
 
 }
