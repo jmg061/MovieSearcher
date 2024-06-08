20c03fdc5dff1d64eb91c37a91b2feae4dd01887
jmg061 <jmg061@inlumine.ual.es>
2024-06-09 00:40:24 +0200

    Docker para MS

--- BACKEND/src/main/java/DRA/MovieSearcher/controller/DBController.java
+++ BACKEND/src/main/java/DRA/MovieSearcher/controller/DBController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/data")
 public class DBController {
